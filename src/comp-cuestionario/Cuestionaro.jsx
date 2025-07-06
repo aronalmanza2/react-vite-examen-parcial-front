64980a1e838a6eb3f5b84688c1377ebd93014197
AronDevLocal <arondevlocal@gmail.com>
2025-07-06 18:12:20 -0500

    correciones finales

--- src/comp-cuestionario/Cuestionaro.jsx
+++ src/comp-cuestionario/Cuestionaro.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { preguntas } from "./data";
 import Pregunta from "./Pregunta";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-icons/font/bootstrap-icons.css"
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Navega from "./comp/Navega.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -11,9 +9,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <Navega>
-        <App />
-      </Navega>
+      <Navega />
     </Router>
   </StrictMode>
 );
